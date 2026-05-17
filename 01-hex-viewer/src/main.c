49d455071378b7d37716c693f1a2d1c64a65c7e2
saeeedhany <alsaeedalbasi0ny@gmail.com>
2026-05-17 22:17:17 +0300

    doing stuff

--- 01-hex-viewer/src/main.c
+++ 01-hex-viewer/src/main.c
@@ -1,18 +1,18 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h>      // for printf()
+#include <unistd.h>     // for read() and close()
+#include <fcntl.h>      // for open() and O_RDONLY
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
     unsigned char buffer[16];
 
     int fd = open(argv[1], O_RDONLY);
 
     ssize_t bytes_read = read(fd, buffer, 16);
 
-    for (ssize_t i = 0; i < bytes_read; i++) {
+    for (ssize_t i =0; i < bytes_read; i++) {
         printf("%02x ", buffer[i]);
-    }
+    };
 
     printf("\n");
 
